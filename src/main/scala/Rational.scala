cc1b221cc5b8f483202f01314e834f3f777bd63e
traumathegod <traumathegod@gmail.com>
2017-12-24 13:16:29 +0100

    - Commenting out gcd function due to bug - Added a main to more easily test application

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -1,11 +1,13 @@
+
+
 class Rational (n: Int, d: Int) {
   require(d != 0)
   def this (n: Int) = this (n, 1) // costruttore ausiliare; se denominatore non è specificato allore è 1
 
-  private val g = gcd(n.abs, d.abs)
+//  private val g = gcd(n.abs, d.abs) TODO: gcd function fails. disabling till it's fixed
 
-  val numeratore: Int = n / g
-  val denominatore: Int = d / g
+  val numeratore: Int = n // / g
+  val denominatore: Int = d // / g
 
   private def gcd (a: Int, b: Int): Int = {
     if (b == 0) a else gcd(b, a & b)
@@ -43,5 +45,27 @@ class Rational (n: Int, d: Int) {
 
   def / (i: Int) : Rational = {
     new Rational(numeratore, denominatore * i)
+  }
+}
+
+object Rational {
+  def main(args: Array[String]):Unit = {
+    println("Inserire nominatore 1")
+    val nom1: Int = io.StdIn.readInt()
+    println("Inserire denominatore 1")
+    val den1: Int = io.StdIn.readInt()
+
+    println("Inserire nominatore 2")
+    val nom2: Int = io.StdIn.readInt()
+    println("Inserire denominatore 2")
+    val den2: Int = io.StdIn.readInt()
+
+    println(s"Frazione 1: $nom1 | $den1 --- Frazione 2: $nom2 | $den2")
+
+    Calcolatore(new Rational(nom1, den1), new Rational(nom2, den2))
+  }
+
+  def Calcolatore(r1: Rational, r2: Rational): Unit = {
+    println(r1 + r2)
   }
 }
